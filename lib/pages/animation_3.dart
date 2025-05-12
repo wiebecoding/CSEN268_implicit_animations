f01bb3a0d64a7a782586a9d32623d340c8d9b537
Brian Wiebe <51461287+wiebecoding@users.noreply.github.com>
2025-05-11 17:08:09 -0700

    Added template for AnimatedRotation

--- lib/pages/animation_3.dart
+++ lib/pages/animation_3.dart
@@ -1,17 +1,37 @@
 import 'package:flutter/material.dart';
 
-class ThirdAnimation extends StatelessWidget {
+class ThirdAnimation extends StatefulWidget {
   const ThirdAnimation({super.key});
 
   @override
+  State<ThirdAnimation> createState() => LogoRotateState();
+}
+
+class LogoRotateState extends State<ThirdAnimation> {
+  double turns = 0.0;
+
+  void _changeRotation() {
+    setState(() => turns += 1.0 / 8.0);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Animation 3: AnimatedRotation'),
-      ),
-      body: const Center(
-        child: Text('Content for Animation 3'),
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        ElevatedButton(
+          onPressed: _changeRotation,
+          child: const Text('Rotate Logo'),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(50),
+          child: AnimatedRotation(
+            turns: turns,
+            duration: const Duration(seconds: 1),
+            child: const FlutterLogo(),
+          ),
+        ),
+      ],
     );
   }
 }
