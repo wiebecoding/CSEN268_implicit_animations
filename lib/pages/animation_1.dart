34606d29f518f17826f374291509cf9b0178398e
Nolan Anderson <120975081+nolanranderson@users.noreply.github.com>
2025-05-11 18:14:32 -0700

    Added AnimatedCrossFade

--- lib/pages/animation_1.dart
+++ lib/pages/animation_1.dart
@@ -6,11 +6,66 @@ class FirstAnimation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Animation 1: AnimatedCrossFade'),
-      ),
-      body: const Center(
-        child: Text('Content for Animation 1'),
+      appBar: AppBar(title: const Text('AnimatedCrossFade Sample')),
+      body: Center(child: AnimatedCrossFadeExample()),
+    );
+  }
+}
+
+class AnimatedCrossFadeExample extends StatefulWidget {
+  AnimatedCrossFadeExample({super.key});
+
+  @override
+  State<AnimatedCrossFadeExample> createState() =>
+      _AnimatedCrossFadeExampleState();
+}
+
+class _AnimatedCrossFadeExampleState extends State<AnimatedCrossFadeExample> {
+  bool first = false;
+  double seconds = 1.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          first = !first;
+        });
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            Center(
+              child: AnimatedCrossFade(
+                duration: Duration(milliseconds: (1000 * seconds).floor()),
+                firstChild: const FlutterLogo(
+                  style: FlutterLogoStyle.horizontal,
+                  size: 250.0,
+                ),
+                secondChild: const FlutterLogo(
+                  style: FlutterLogoStyle.stacked,
+                  size: 250.0,
+                ),
+                crossFadeState:
+                    first
+                        ? CrossFadeState.showFirst
+                        : CrossFadeState.showSecond,
+              ),
+            ),
+            SizedBox(height: 16.0),
+            Slider(
+              min: 0,
+              max: 3.0,
+              value: seconds,
+              onChanged: (double value) {
+                setState(() {
+                  seconds = value;
+                });
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
