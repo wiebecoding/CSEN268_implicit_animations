7d2f2438a5fb9fc9d74d9b0eb8e5e2b7fea6d88e
Nolan Anderson <120975081+nolanranderson@users.noreply.github.com>
2025-05-11 18:01:43 -0700

    Added slider and toggle button to AnimatedAlign

--- lib/pages/animation_2.dart
+++ lib/pages/animation_2.dart
@@ -3,30 +3,17 @@ import 'package:flutter/material.dart';
 class SecondAnimation extends StatelessWidget {
   const SecondAnimation({super.key});
 
-  static const Duration duration = Duration(seconds: 1);
-  static const Curve curve = Curves.fastOutSlowIn;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Animation 2: AnimatedAlign')),
-      body: const Center(
-        child: AnimatedAlignExample(curve: curve, duration: duration),
-      ),
+      appBar: AppBar(title: const Text('AnimatedAlign Sample')),
+      body: Center(child: AnimatedAlignExample()),
     );
   }
 }
 
 class AnimatedAlignExample extends StatefulWidget {
-  const AnimatedAlignExample({
-    required this.duration,
-    required this.curve,
-    super.key,
-  });
-
-  final Duration duration;
-
-  final Curve curve;
+  AnimatedAlignExample({super.key});
 
   @override
   State<AnimatedAlignExample> createState() => _AnimatedAlignExampleState();
@@ -34,6 +21,10 @@ class AnimatedAlignExample extends StatefulWidget {
 
 class _AnimatedAlignExampleState extends State<AnimatedAlignExample> {
   bool selected = false;
+  double seconds = 1.0;
+  int current = 0;
+
+  List<Curve> curves = [Curves.fastOutSlowIn, Curves.bounceInOut];
 
   @override
   Widget build(BuildContext context) {
@@ -43,17 +34,45 @@ class _AnimatedAlignExampleState extends State<AnimatedAlignExample> {
           selected = !selected;
         });
       },
-      child: Center(
-        child: Container(
-          width: 250.0,
-          height: 250.0,
-          color: Colors.red,
-          child: AnimatedAlign(
-            alignment: selected ? Alignment.topRight : Alignment.bottomLeft,
-            duration: widget.duration,
-            curve: widget.curve,
-            child: const FlutterLogo(size: 50.0),
-          ),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            Center(
+              child: Container(
+                width: 250.0,
+                height: 250.0,
+                color: Colors.blueGrey,
+                child: AnimatedAlign(
+                  alignment:
+                      selected ? Alignment.topRight : Alignment.bottomLeft,
+                  duration: Duration(milliseconds: (1000 * seconds).floor()),
+                  curve: curves[current],
+                  child: const FlutterLogo(size: 50.0),
+                ),
+              ),
+            ),
+            SizedBox(height: 16.0),
+            Slider(
+              min: 0,
+              max: 5.0,
+              value: seconds,
+              onChanged: (double value) {
+                setState(() {
+                  seconds = value;
+                });
+              },
+            ),
+            SizedBox(height: 16.0),
+            FilledButton(
+              onPressed: () {
+                setState(() {
+                  current = ((current + 1) % curves.length).floor();
+                });
+              },
+              child: Text("Toggle Curve"),
+            ),
+          ],
         ),
       ),
     );
