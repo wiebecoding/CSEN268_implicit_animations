1285d9155620aef6fd61865d0f2c4816664d455f
Nolan Anderson <120975081+nolanranderson@users.noreply.github.com>
2025-05-11 17:12:29 -0700

    Added template for AnimatedAlign

--- lib/pages/animation_2.dart
+++ lib/pages/animation_2.dart
@@ -3,14 +3,58 @@ import 'package:flutter/material.dart';
 class SecondAnimation extends StatelessWidget {
   const SecondAnimation({super.key});
 
+  static const Duration duration = Duration(seconds: 1);
+  static const Curve curve = Curves.fastOutSlowIn;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Animation 2: AnimatedAlign'),
-      ),
+      appBar: AppBar(title: const Text('Animation 2: AnimatedAlign')),
       body: const Center(
-        child: Text('Content for Animation 2'),
+        child: AnimatedAlignExample(curve: curve, duration: duration),
+      ),
+    );
+  }
+}
+
+class AnimatedAlignExample extends StatefulWidget {
+  const AnimatedAlignExample({
+    required this.duration,
+    required this.curve,
+    super.key,
+  });
+
+  final Duration duration;
+
+  final Curve curve;
+
+  @override
+  State<AnimatedAlignExample> createState() => _AnimatedAlignExampleState();
+}
+
+class _AnimatedAlignExampleState extends State<AnimatedAlignExample> {
+  bool selected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selected = !selected;
+        });
+      },
+      child: Center(
+        child: Container(
+          width: 250.0,
+          height: 250.0,
+          color: Colors.red,
+          child: AnimatedAlign(
+            alignment: selected ? Alignment.topRight : Alignment.bottomLeft,
+            duration: widget.duration,
+            curve: widget.curve,
+            child: const FlutterLogo(size: 50.0),
+          ),
+        ),
       ),
     );
   }
